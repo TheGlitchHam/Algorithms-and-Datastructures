4213ba73c9efa979ea39ecba6416ce889e9632b5
TheGlitchHam <faross@online.de>
2021-07-31 20:11:24 +0200

    Finishing quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,5 +1,21 @@
 import sys
 from load import load_numbers
+import time
+"""
+Quicksort algorithm:
+Pick a pivot (random value from list)
+Split the list into two sublists, one with values smaller or equal,
+one with values greater than pivot
+Through recursion, this happens with every sublist, too
+In the end, we simply return the two sublists with their recursive calls, and
+merge it with the pivot at the middle 
+Steps:
+1. Pick pivot
+2. Split into two sublists, [] <= pivot < []
+3. Recursive quicksort on both sublists
+4. Return quicksorted lists and merge them with pivot in the middle
+"""
+
 
 numbers = load_numbers(sys.argv[1])
 
@@ -16,8 +32,12 @@ def quicksort(values):
             less_than_pivot.append(value)
         else:
             greater_than_pivot.append(value)
-    print("%15s %1s %-15s" % (less_than_pivot, pivot, greater_than_pivot))
+    # print("%15s %1s %-15s" % (less_than_pivot, pivot, greater_than_pivot))
     return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
 
 
-print(quicksort(numbers))
+start = time.time()
+result = (quicksort(numbers))
+end = time.time()
+print(result)
+print("Time: ", (end - start))
